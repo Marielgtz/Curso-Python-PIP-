b8997573063c0e88d3f87e602b27ebe9f44576c3
Mariel Gtz <99166542+Marielgtz@users.noreply.github.com>
2025-09-11 00:49:48 -0600

    Update 01_print.py

--- 01_print.py
+++ 01_print.py
@@ -1,3 +1,5 @@
+#Clase uno variables, funciones y sintaxis básica
+
 print("Hola, esto es el archivo 01")
 
 print("Hola soy Nicolas y tengo 12 años")
@@ -8,3 +10,14 @@ print(12 + 5)
 print(10 - 5)
 print(2 * 3)
 print(8 / 2)
+
+# Una variable se define e inicializa en una sola línea, no es necesario declarar el tipo de datos de una variable, 
+# ya que el lenguaje determina el tipo de datos en tiempo de ejecución basándose en el valor que se le asigna
+# Variables que pueden cambiar con el tiempo:
+
+edad = 30
+hora = 12 
+
+
+
+
